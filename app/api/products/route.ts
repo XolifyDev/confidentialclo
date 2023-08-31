b3253bdcd7b3f0aaf3e7e4d4ae42cb04717c5064
Xolify <103285682+XolifyDev@users.noreply.github.com>
2023-08-31 15:07:04 -0400

    awdawd

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,7 +2,8 @@ import { prisma } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const product = await prisma.products.findMany();
   return NextResponse.json({
-    products: await prisma.products.findMany(),
+    products: product,
   });
 }
