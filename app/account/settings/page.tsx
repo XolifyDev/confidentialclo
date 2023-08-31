a3a778b1b49ca51fcc43c4c9d7bddaf3c97224ab
Xolify <103285682+XolifyDev@users.noreply.github.com>
2023-08-31 12:20:54 -0400

    Update page.tsx

--- app/account/settings/page.tsx
+++ app/account/settings/page.tsx
@@ -160,7 +160,7 @@ const Page = () => {
                                         Notifications
                                     </h2>
                                     <p className="mt-1 text-sm leading-6 text-gray-600">
-                                        We'll always let you know about important changes, but you pick what
+                                        We&apos;ll always let you know about important changes, but you pick what
                                         else you want to hear about.
                                     </p>
                                     <div className="mt-10 space-y-10">
